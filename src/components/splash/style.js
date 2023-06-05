28ebe89c1be509733346d929d4a3461a91bacda6
AyaAdel-tap <a.adel@tap.company>
2023-06-05 03:59:45 +0300

    Fix Splash height for IOS

--- src/components/splash/style.js
+++ src/components/splash/style.js
@@ -1,7 +1,11 @@
 import Styled from 'styled-components';
 
 export const SplashWrapper = Styled.div`
-  height: 100vh;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
   display: flex;
   align-items: center;
   justify-content: center;
